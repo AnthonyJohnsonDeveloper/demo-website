b25ebbcc1b5da3d32cf04845804f4279425787d3
Anthony <anthonyjohnson5016@gmail.com>
2024-05-22 21:51:21 -0400

    Just about finished, home page is done only a few more to go

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,23 +1,32 @@
-
-import img from "../assets/img/home.png"
-import Button from '../layouts/button'
+import img from "../assets/img/home1.png";
+import Button from "../layouts/button";
 function Home() {
   return (
-    <div className='min-h-screen flex flex-col justify-center lg:flex-row lg:justify-center items-center lg:px-32 px-5 gap-10 bg-gradient-to-r from-[#FFDCAB] to-[#AB6B2E]'>
-        <div className='w-full lg:w-2/4 space-y-4 mt-14 lg:mt-0'>
-            <h1 className='font-semibold text-5xl text-center lg:text-start leading-tight'>Lorem ipsum dolor sit amet.</h1>
-            <p>Lorem ipsum dolor sit amet.</p>
+    <div className=" min-h-screen flex flex-col justify-center lg:flex-row lg:justify-between items-center lg:px-32 px-5 gap-10 bg-gradient-to-r from-[#FFDCAB] to-[#AB6B2E] ">
+      <div className=" w-full lg:w-2/4 space-y-4 mt-14 lg:mt-0">
+        <h1 className="font-semibold text-5xl text-center lg:text-start leading-tight">
+          Lorem ipsum dolor sit amet.
+        </h1>
+        <p>Lorem ipsum dolor sit amet.</p>
 
-            <div className='flex flex-row gap-6'>
-                <Button title="Add to Cart" />
-                <Button title="More Menu" />
-            </div>
+        <div className="flex flex-row gap-6">
+          <Button title="Add to Cart" />
+          <Button title="More Menu" />
         </div>
-        <div>
-            <img src={img} alt="img" />
+      </div>
+      <div className="relative">
+        <img src={img} alt="img" />
+
+        <div className="absolute bg-white px-8 py-2 top-5 right-0 rounded-full shadow-[0_20px_50px_rgba(8,_112,_184,_0.7)]">
+          <h2 className="font-semibold">14k</h2>
         </div>
+
+        <div className="absolute bg-white px-8 py-2 bottom-0 -left-10 rounded-full">
+          <h2>Lorem ipsum</h2>
+          </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
---
+++ src/Components/Menu.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Menu = () => {
+    return (
+        <div>
+            <h1>Our Menu</h1>
+
+            <div>
+                
+            </div>
+        </div>
+    )
+}
+
+export default Menu
